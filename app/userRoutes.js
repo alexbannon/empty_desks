fdd3a19460dbcf9bc2955fa326a80444394a985b
Alex Bannon <alexbannon@gmail.com>
2015-09-21 16:33:29 -0400

    crud functionality on users with minor styling

--- app/userRoutes.js
+++ app/userRoutes.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var User = require('./models/user');
 var path = require("path");
+var bcrypt = require('bcryptjs');
 
 router.get('/api/users', function(req, res) {
     User.find(function(err, users) {
@@ -33,18 +34,29 @@ router.get("/api/users/:id", function(req, res){
 
 router.put("/api/users/:id", function(req, res){
   if(req.user._id == req.params.id){
-    User.findOneAndUpdate({ "_id": req.params.id}, req.body, { 'new': true}, function(err, user){
-      if (err) return handleError(err);
-      res.send(user)
-    })
+    console.log(req.session.passport.user)
+    if (bcrypt.compareSync(req.body.password, req.body.password_digest)) {
+      var user = {
+        "username": req.body.username,
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "email": req.body.email,
+        "avatar_url": req.body.avatar_url,
+        "password_digest": req.body.password_digest
+      }
+      User.findOneAndUpdate({ "_id": req.params.id}, user, { 'new': true}, function(err, user){
+        if (err) return handleError(err);
+        req.session.passport.user = user
+        res.send(user)
+      })
+    }
+    else {
+      res.send({"error": "incorrect password"})
+    }
   }
   else {
-    res.send("not authorized")
+    res.send({"error": "not authorized"})
   }
-  Desk.findOneAndUpdate({ "_id": req.params.id}, req.body, { 'new': true }, function(err, desk){
-    if (err) return handleError(err);
-    res.send(desk);
-  })
 })
 
 
--- public/js/controllers/users.js
+++ public/js/controllers/users.js
@@ -1,11 +1,23 @@
 (function() {
   var userControllers = angular.module('userControllers', ['ngRoute'])
 
-  userControllers.controller('editUserController', ['User', 'AuthService', function(User, AuthService) {
+  userControllers.controller('editUserController', ['User', 'AuthService', '$window', function(User, AuthService, $window) {
     var self = this;
     AuthService.current_user().then(function(response){
       self.user = [response.data]
     });
+    this.updateUser = function(){
+      User.update({id: self.user[0]._id}, self.user[0], function(response){
+        $window.location.reload()
+      })
+    }
+    this.resetUser = function(){
+      this.editToggle = !this.editToggle;
+      AuthService.current_user().then(function(response){
+        self.user = [response.data]
+      });
+    }
+
   }]);
 
 })();
