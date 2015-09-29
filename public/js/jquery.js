15817464693077f7f7e9cb6bbeaf7cbcb12482b4
Alex Bannon <alexbannon@gmail.com>
2015-09-28 23:40:57 -0400

    styling signup and sign in page and background

--- public/js/jquery.js
+++ public/js/jquery.js
@@ -17,4 +17,30 @@ $(document).ready(function(){
     window.location.replace("/#/search_results/"+search_term)
     $(".search_input").val("");
   })
+  resizeDiv();
+
+  window.onresize = function(event) {
+    resizeDiv();
+  }
+
+  function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height() - 80;
+    $('#background').css({'height': vph + 'px'});
+    $("#overlay_signin").css({'height': vph + 'px'});
+  }
+  $("sign_up_window").hide();
+  $("#sign_in").css("background-color", "rgb(40, 32, 111)");
+  $("#sign_up").click(function(){
+    $("#sign_in").css("background-color", "rgb(30, 62, 141)");
+    $("#sign_up").css("background-color", "rgb(40, 32, 111)");
+    $("#sign_in_window").fadeOut();
+    $("#sign_up_window").fadeIn();
+  })
+  $("#sign_in").click(function(){
+    $("#sign_up").css("background-color", "rgb(30, 62, 141)")
+    $("#sign_in").css("background-color", "rgb(40, 32, 111)")
+    $("#sign_in_window").fadeIn();
+    $("#sign_up_window").fadeOut();
+  })
 })
